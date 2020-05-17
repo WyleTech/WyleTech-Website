4333f3933d75ee5434095cb3de66ad3e1a9c9960
Squirrel Master <22codingsm@gmail.com>
2020-05-16 22:07:06 -0700

    V. 1.6 - Projects Page
    - Added new photos of previous work for the projects page (Name Game and Ril Dil). Added them to React/Bootstrap cards to showcase work.
    
    - Added new photos for React/Java and Java and Angular/API - added them to the cards wih showcased work.
    
    - Adding Github info to third card until our Bowling App is ready to showcase.
    
    **ISSUES** Projects page is overlapping with present footer. Need to fix alignment issue.

--- wyletech-website/src/Component/Projects.js
+++ wyletech-website/src/Component/Projects.js
@@ -3,59 +3,67 @@ import { Link } from "react-router-dom";
 import CardDeck from "react-bootstrap/CardDeck";
 import Card from 'react-bootstrap/Card';
 import ReactLogo from '../images/ReactLogo.png';
-import AngularLogo from '../images/Angular.png';
-
+import AngularApi from '../images/AngularApi.jpg';
+import JavaReact from '../images/JavaReact.png';
+import RilDilSnip from '../images/RilDilSnip.jpg';
+import NameGame from '../images/NameGame.jpg';
 //import CardColumns from "react-bootstrap/CardColumns";
 
 const Projects = () => (
   <React.Fragment>
-     <body className="Projects-Body">
-     <header className="App-header">
-     <h1 className="Our-Creations-Title">Our Creations</h1>
+    <body className="Projects-Body ">
+      <header className="App-header">
+        <h1 className="Our-Creations-Title">Our Creations</h1>
 
-    {/* React Bootstrap Project examples */}
-    <CardDeck>
-  <Card>
-    <Card.Img variant="top" src={ReactLogo} style= {{height: 500}}/>
-    <Card.Body>
-      <Card.Title className="Card-Titles text-center">React and Java Build</Card.Title>
-      <Card.Text> 
-        <a className="Project-names text-info" href=
-      "http://gcufoodtruck.us-west-1.elasticbeanstalk.com/"> Ril Dil BBQ
+        {/* React Bootstrap Project examples */}
+        <CardDeck>
+          <Card>
+            <br></br>
+            <Card.Img variant="top" src={RilDilSnip} style={{ height: 250, width: 610 }} />
+            <br></br>
+            <Card.Img variant="top" src={JavaReact} style={{ height: 250, width: 610 }} />
+            <Card.Body>
+              <Card.Title className="Card-Titles text-center">React and Java Build</Card.Title>
+              <Card.Text>
+                <a className="Project-names text-info" href=
+                  "http://gcufoodtruck.us-west-1.elasticbeanstalk.com/"> Ril Dil BBQ
         </a>
-      </Card.Text>
-    </Card.Body>
+              </Card.Text>
+            </Card.Body>
 
-  </Card>
-  <Card>
-    <Card.Img  variant="top" src={AngularLogo} style= {{height: 500}} />
-    <Card.Body>
-      <Card.Title className="Card-Titles" >Angular Build with API consumption</Card.Title>
-      <Card.Text>
-      <a className="Project-names text-danger" href=
-      "http://name-game.us-west-1.elasticbeanstalk.com/aboutUs"> Name Game
+          </Card>
+          <Card>
+            <br></br>
+            <Card.Img variant="top" src={NameGame} style={{ height: 250, width: 610 }} />
+            <br></br>
+            <Card.Img variant="top" src={AngularApi} style={{ height: 250, width: 610 }} />
+            <Card.Body>
+              <Card.Title className="Card-Titles" >Angular Build with API consumption</Card.Title>
+              <Card.Text>
+                <a className="Project-names text-danger" href=
+                  "http://name-game.us-west-1.elasticbeanstalk.com/aboutUs"> Name Game
         </a>
-      </Card.Text>
-    </Card.Body>
+              </Card.Text>
+            </Card.Body>
 
-  </Card>
-  <Card>
-    <Card.Img variant="top" src="holder.js/100px160" />
-    <Card.Body>
-      <Card.Title className="Card-Titles">Preceptor-Point Site Build</Card.Title>
-      <Card.Text>
+          </Card>
+          <Card>
+            <Card.Img variant="top" />
+            <Card.Body>
+              <Card.Title className="Card-Titles">Check out our Github</Card.Title>
+              <Card.Text>
 
-      <a className="Project-names text-danger" href=
-      "http://www.preceptorpoint.com"> Preceptor-Point
+                <a className="Project-names text-danger" href=
+                  "https://github.com/WyleTech"> WyleTech Github
         </a>
-     Site build for Preceptor-point including React/Bootstrap styling, MYSQL RDS Database on AWS, React form-validation, and Java Backend. 
-      </Card.Text>
-    </Card.Body>
 
-  </Card>
-</CardDeck>
-</header>
-</body>
+              </Card.Text>
+            </Card.Body>
+
+          </Card>
+        </CardDeck>
+      </header>
+    </body>
 
   </React.Fragment>
 );
