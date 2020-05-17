e1f152d7c141c63e55ae9d3474133c537a76b9ed
Squirrel Master <22codingsm@gmail.com>
2020-05-16 22:42:12 -0700

    V. 1.6.1 - Projects Page Edits and Next Steps
    - More Edits to the Project Page. 3
    
    Next Step for Projects Page:
    - Need to choose a color scheme with Bill for the page.
    
    - Add third project once Bowling App is prepared. Maybe just get link to Github up there for now since no one will see it in short term??  Consider Creating a front-end soon so we can get a snippett for the Projects Page

--- wyletech-website/src/Component/Projects.js
+++ wyletech-website/src/Component/Projects.js
@@ -12,57 +12,52 @@ import NameGame from '../images/NameGame.jpg';
 const Projects = () => (
   <React.Fragment>
     <body className="Projects-Body ">
-      <header className="App-header">
-        <h1 className="Our-Creations-Title">Our Creations</h1>
+      <h1 className="Our-Creations-Title">Our Creations</h1>
+      {/* React Bootstrap Project examples */}
+      <CardDeck >
+        <Card className="Card-1 bg-primary">
+          <br></br>
+          <Card.Img variant="top" src={RilDilSnip} style={{ height: 250, width: 610 }} />
+          <p className="Ril-Dil-Card-Description text-center font-weight-bold">Ril Dil Food-Truck Website - Build with Menu, Google Maps API for Food Truck Locator, Menu Photos Carousel, Form for Client Booking, admin login for Food-Truck Owner to acitvely add/remove menu items and maintain database of client bookings. </p>
+          <Card.Img variant="top" src={JavaReact} style={{ height: 250, width: 610 }} />
+          <Card.Body>
+            <Card.Title className="Card-Titles text-center">React, Bootstrap, Java Site-Build. MYSQL Database on AWS. Form Validation allowing Client Booking</Card.Title>
+            <Card.Text>
+              {/* <a className="Project-names text-info" href=
+                "http://gcufoodtruck.us-west-1.elasticbeanstalk.com/"> Ril Dil BBQ
+              </a> */}
+            </Card.Text>
+          </Card.Body>
 
-        {/* React Bootstrap Project examples */}
-        <CardDeck>
-          <Card>
-            <br></br>
-            <Card.Img variant="top" src={RilDilSnip} style={{ height: 250, width: 610 }} />
-            <br></br>
-            <Card.Img variant="top" src={JavaReact} style={{ height: 250, width: 610 }} />
-            <Card.Body>
-              <Card.Title className="Card-Titles text-center">React and Java Build</Card.Title>
-              <Card.Text>
-                <a className="Project-names text-info" href=
-                  "http://gcufoodtruck.us-west-1.elasticbeanstalk.com/"> Ril Dil BBQ
+        </Card >
+        <Card className="Card-2 bg-primary">
+          <br></br>
+          <Card.Img variant="top" src={NameGame} style={{ height: 250, width: 610 }} />
+          <br></br>
+          <Card.Img variant="top" src={AngularApi} style={{ height: 250, width: 610 }} />
+          <Card.Body>
+            <Card.Title className="Card-Titles" >"The Name Game" Angular Build with API consumption</Card.Title>
+            <Card.Text>
+              {/* <a className="Project-names text-danger" href=
+                "http://name-game.us-west-1.elasticbeanstalk.com/aboutUs"> Name Game
+              </a> */}
+            </Card.Text>
+          </Card.Body>
+        </Card>
+        <Card className="Card-3 bg-primary">
+          <Card.Img variant="top" />
+          <Card.Body>
+            <Card.Title className="Card-Titles">Check out our Github</Card.Title>
+            <Card.Text>
+
+              <a className="Project-names text-danger" href=
+                "https://github.com/WyleTech"> WyleTech Github
         </a>
-              </Card.Text>
-            </Card.Body>
+            </Card.Text>
+          </Card.Body>
+        </Card>
 
-          </Card>
-          <Card>
-            <br></br>
-            <Card.Img variant="top" src={NameGame} style={{ height: 250, width: 610 }} />
-            <br></br>
-            <Card.Img variant="top" src={AngularApi} style={{ height: 250, width: 610 }} />
-            <Card.Body>
-              <Card.Title className="Card-Titles" >Angular Build with API consumption</Card.Title>
-              <Card.Text>
-                <a className="Project-names text-danger" href=
-                  "http://name-game.us-west-1.elasticbeanstalk.com/aboutUs"> Name Game
-        </a>
-              </Card.Text>
-            </Card.Body>
-
-          </Card>
-          <Card>
-            <Card.Img variant="top" />
-            <Card.Body>
-              <Card.Title className="Card-Titles">Check out our Github</Card.Title>
-              <Card.Text>
-
-                <a className="Project-names text-danger" href=
-                  "https://github.com/WyleTech"> WyleTech Github
-        </a>
-
-              </Card.Text>
-            </Card.Body>
-
-          </Card>
-        </CardDeck>
-      </header>
+      </CardDeck>
     </body>
 
   </React.Fragment>
