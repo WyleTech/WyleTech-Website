3b499ccff86f3febb200873bcebc385d26e805e1
Squirrel Master <22codingsm@gmail.com>
2020-08-10 15:59:43 -0700

    Front-End Work (Projects-Page)
    - Worked on React/Bootstrap Cards for "Projects" page.
    - Specifically added a React/Bootstrap ListGroup aned ListGroup Items for styling in Ril Dil Food Truck Card.
    
    (Future Plans) style all cards to reflect new styling with React/Bootstrap lists.  Add new projects with HD images to showcase.
    
    **Important Styling note** need to work on responsiveness of Projects page for Mobile/Tablet.

--- wyletech-website/src/Component/Projects.js
+++ wyletech-website/src/Component/Projects.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Link } from "react-router-dom";
 import CardDeck from "react-bootstrap/CardDeck";
 import Card from 'react-bootstrap/Card';
-import ReactLogo from '../images/ReactLogo.png';
 import AngularApi from '../images/AngularApi.jpg';
 import JavaReact from '../images/JavaReact.png';
 import RilDilSnip from '../images/RilDilSnip.jpg';
 import NameGame from '../images/NameGame.jpg';
+import ListGroup from 'react-bootstrap/ListGroup'
+import ListGroupItem from 'react-bootstrap/ListGroupItem'
 //import CardColumns from "react-bootstrap/CardColumns";
 
 const Projects = () => (
@@ -18,16 +19,28 @@ const Projects = () => (
         <Card className="Card-1 bg-info">
           <br></br>
           <Card.Img variant="top" src={RilDilSnip} style={{ height: 250, width: 610 }} />
-          <p className="Ril-Dil-Card-Description text-center font-weight-bold">Ril Dil Food-Truck Website - Build with Menu, Google Maps API for Food Truck Locator, Menu Photos Carousel, Form for Client Booking, admin login for Food-Truck Owner to acitvely add/remove menu items and maintain database of client bookings. </p>
+          <Card.Body>
+             {/**CSS Card.Title note** won't style "className" (on CSS) like normal react, likely because it's a react/bootstrap listgroup - would only style under "class" */}
+            <Card.Title class= "Card-1-Title bg-dark text-white text-xl-center">Ril Dil Food-Truck Website</Card.Title> 
+            <Card.Text className= "bg-dark text-white text-center">
+              Tools: Java, MYSQL, Amazon Web Services AWS, Google API, HTML/CSS/React/Bootstrap, Form Validation. 
+    </Card.Text>
+          </Card.Body>
+          <ListGroup className="list-group-flush bg-info">
+            <ListGroupItem className="bg-dark text-white">Responsive Menu - Photo Carousel </ListGroupItem>
+            <ListGroupItem className="bg-dark text-white">Google Maps API for Food Truck Locator</ListGroupItem>
+            <ListGroupItem className="bg-dark text-white">React/Bootstrap Form for Client Booking</ListGroupItem>
+            <ListGroupItem className="bg-dark text-white">Admin login for Owner - Add/Remove Menu Items/Photos, Input GPS Coordinates for Food Truck Locator, Maintain database of client booking/contact info</ListGroupItem>
+          </ListGroup>
+          <br></br>
           <Card.Img variant="top" src={JavaReact} style={{ height: 250, width: 610 }} />
           <Card.Body>
-            <Card.Title className="Card-Titles text-center">React, Bootstrap, Java Site-Build. MYSQL Database on AWS. Form Validation allowing Client Booking</Card.Title>
-            <Card.Text>
-              {/* <a className="Project-names text-info" href=
-                "http://gcufoodtruck.us-west-1.elasticbeanstalk.com/"> Ril Dil BBQ
-              </a> */}
-            </Card.Text>
+            <Card.Link href="#">Card Link</Card.Link>
           </Card.Body>
+{/* NOW!  */}
+
+
+
 
         </Card >
         <Card className="Card-2 bg-info">
